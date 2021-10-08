1ea65e8e2b80ef6d36741499b94dc9af2a026418
lokeshbeere <54480489+lokeshbeere@users.noreply.github.com>
2021-10-08 17:09:46 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role_policy" "test_policy" {
   role = aws_iam_role.test_role.id
 
   # Terraform's "jsonencode" function converts aa
-  # Terraform expression result to valid JSON syntax..
+  # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
