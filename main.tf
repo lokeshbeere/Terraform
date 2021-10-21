afa2add0615638b9eb6d1695a655d621fe25bf97
lokeshbeere <54480489+lokeshbeere@users.noreply.github.com>
2021-10-21 12:52:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -320,6 +320,8 @@ resource "aws_s3_bucket_public_access_block" "public" {
 
   block_public_acls   = true
   block_public_policy = true
+  
+  depends_on = [
+    aws_s3_bucket.bucket
+  ]
 }
-
-## upload data to s3 bucket
